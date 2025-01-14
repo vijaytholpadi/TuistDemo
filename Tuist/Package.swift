79e97808d0e9b852fab27947e2438c02865d97e9
Vijay Tholpadi <vijay@thegeekprojekt.com>
2025-01-14 22:35:07 +0800

    Adds more dependencies

--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -17,6 +17,13 @@ let package = Package(
     dependencies: [
         // Add your own dependencies here:
          .package(url: "https://github.com/Alamofire/Alamofire", from: "5.0.0"),
+         .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.0.0")),
+         .package(url: "https://github.com/SDWebImage/SDWebImage.git", .upToNextMajor(from: "5.0.0")),
+         .package(url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.0.0")),
+         .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.0.0")),
+         .package(url: "https://github.com/airbnb/lottie-ios.git", .upToNextMajor(from: "4.0.0")),
+         .package(url: "https://github.com/malcommac/SwiftDate.git", .upToNextMajor(from: "7.0.0")),
+         .package(url: "https://github.com/danielgindi/Charts.git", .upToNextMajor(from: "4.0.0"))
         // You can read more about dependencies here: https://docs.tuist.io/documentation/tuist/dependencies
     ]
 )
